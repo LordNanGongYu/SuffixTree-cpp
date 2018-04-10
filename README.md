439216f916ed3aa2555cf6862ff44b4f17292eb8
longbaolin <longbaolin@meituan.com>
2018-04-10 12:49:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SuffixTree C++ Version
---
+++ cppSuffixTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppSuffixTree
+//
+//  Created by longbaolin on 2018/4/10.
+//  Copyright © 2018年 longbaolin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
